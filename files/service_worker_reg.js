fcdf7bb1aff55a04f7a738612ce4f0da64df1825
Nicholas Griffin <nicholasgriffin@technutty.co.uk>
2016-09-13 23:14:32 +0100

    Added comments where i previously forgot to.

--- files/service_worker_reg.js
+++ files/service_worker_reg.js
@@ -5,13 +5,14 @@ if ('serviceWorker' in navigator) {
 
     console.log("Browser supports Service Worker");
 
-    var el = document.getElementById('InstallServiceWorkerBanner');
+    // var el = document.getElementById('InstallServiceWorkerBanner');
     var bl = document.getElementById('InstalledServiceWorkerBanner');
+    // var ul = document.getElementById('UpdatedServiceWorkerBanner');
 
 console.log('About to try to install a Service Worker');
 
 navigator.serviceWorker.register('/serviceworker.js?v=1.0.1.1.2', {
-  scope: '.'
+  scope: '/'
 }).then(function(registration) {
   console.log('The service worker has been registered ', registration);
 });
@@ -32,10 +33,16 @@ navigator.serviceWorker.addEventListener('controllerchange', function(event) {
         console.log("Current Service Worker state: Activated");
         bl.style.display = 'block'; 
     }     
-    if (this.state === 'null') {
-    console.log("No Service Worker active...");
-    el.style.display = 'block';  
-    }
+    // This would be for if the service worker is not installed, but that state does not exist, I am still looking for a way to change that.
+   // if (this.state === 'null') {
+   // console.log("No Service Worker active...");
+   // el.style.display = 'block';  
+   // }
+    // I would also like an update state, the code for that would be something like the following:
+      // if (this.state === 'null') {
+   // console.log("Current Service Worker state: Updating);
+   // ul.style.display = 'block';  
+   // }
    }
   );
 });
