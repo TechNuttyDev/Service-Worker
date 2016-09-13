fb4bf2ac0d8e2f8e62b7edd49308d91490fb8919
Nicholas Griffin <nicholasgriffin@technutty.co.uk>
2016-09-13 22:33:19 +0100

    Changed route for getting SW activation
    Now works, but not in the original way that I was thinking originally.
    Slightly worse method in my opinion.

--- files/service_worker_reg.js
+++ files/service_worker_reg.js
@@ -1,48 +1,44 @@
-<script>
-
 // Service Worker
 
 if ('serviceWorker' in navigator) {
     // Check for service worker
 
     console.log("Browser supports Service Worker");
-    if (navigator.serviceWorker.current) {
-    // If service worker is already installed.
-        console.log("Current Service Worker state: \\o/");
-    } else {
-    // If service has not been installed.
+
+    var el = document.getElementById('InstallServiceWorkerBanner');
+    var bl = document.getElementById('InstalledServiceWorkerBanner');
+
+console.log('About to try to install a Service Worker');
+
+navigator.serviceWorker.register('/serviceworker.js?v=1.0.1.1.2', {
+  scope: '.'
+}).then(function(registration) {
+  console.log('The service worker has been registered ', registration);
+});
+
+navigator.serviceWorker.addEventListener('controllerchange', function(event) {
+  console.log(
+    '[controllerchange] A "controllerchange" event has happened ' +
+    'within navigator.serviceWorker: ', event
+  );
+
+  navigator.serviceWorker.controller.addEventListener('statechange',
+    function() {
+      console.log('[controllerchange][statechange] ' +
+        'A "statechange" has occured: ', this.state
+      );
+        // If service worker is already installed.
+     if (this.state === 'activated') {
+        console.log("Current Service Worker state: Activated");
+        bl.style.display = 'block'; 
+    }     
+    if (this.state === 'null') {
     console.log("No Service Worker active...");
-var el = document.getElementById('InstallServiceWorkerBanner');
-var body = document.getElementsByTagName('body');
-el.style.display = 'block';  
-
+    el.style.display = 'block';  
     }
-    window.onload=function(){
-
-    // Service worker install script.
-
-    document.getElementById('tn-offline-install').addEventListener('click', function () {
-        console.log('About to try to install a Service Worker');
-
-        navigator.serviceWorker.register('/serviceworker.js?v=1.0.1.1.2', {
-            scope: '/'
-        })
-        .then(function (serviceWorker) {
-           register = serviceWorker;
-            document.getElementsByTagName('body').innerHTML='<div id="offline-notification-wrapper"><div id="offlineNotification"><span>Caching complete! Future visits will work offline.</span> <a class="close-offline-banner" style="font-size: 14px;font-weight: 800; href="javascript:void(0);" onclick="removeMeOffline();"><span>X</span></a></div></div>';
-            console.log('Successfully installed ServiceWorker');
-            serviceWorker.addEventListener('statechange', function (event) {
-            console.log("Worker state is now " + event.target.state);
-        });
-    }, function (why) {
-        console.log('Failed to install:' + why);
-    });
-    }); // Install button
-
-    }  // Onload
-
+   }
+  );
+});
 } else {
     log("This browser does not support Service Worker.");
 }
-
-</script>
