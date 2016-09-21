b5c258a5fa736c792ba7d5ad0b2ff05e8430f344
Nicholas Griffin <nicholasgriffin@technutty.co.uk>
2016-09-21 23:28:58 +0100

    Updated service worker.
    Includes more caching, more functionality, and more offline fallbacks.

--- sw-toolbox service worker/sw.js
+++ sw-toolbox service worker/sw.js
@@ -1,280 +1,183 @@
-(global => {
-  'use strict';
-  
-// Service Worker Script
-
-// Import the SW Toolbox library
-
-console.log('Service Worker Enabled')
-
-importScripts('sw-toolbox.js');
-
-// Precache critical resources
-
- const FILES_TO_PRECACHE = [
-	'/',
-	'https://technutty.co.uk/TechNuttyLogo.svg',
-	'https://technutty.co.uk/TechNuttyLogo.png',
-	'/offline',
-	'https://technutty.co.uk/wp-content/themes/TechNutty5-5/library/css/stylesheet.shorthand.min.css',
-	'https://technutty.co.uk/wp-includes/js/jquery/jquery.js',
-	'https://technutty.co.uk/wp-includes/js/jquery/jquery-migrate.min.js',
-	'https://technutty.co.uk/modernizr-custom.js',
-	'https://technutty.co.uk/topics/articles/all/news/',
-	'https://technutty.co.uk/topics/articles/all/reviews/'
-];
-
-global.toolbox.precache(FILES_TO_PRECACHE);
-
-// Turn on debug logging, visible in the Developer Tools' console.
-global.toolbox.options.debug = false;
- 
-// Boilerplate to ensure our service worker takes control of the page 
-// as soon as possible.
-  global.addEventListener('install',
-      event => event.waitUntil(global.skipWaiting()));
-  global.addEventListener('activate',
-      event => event.waitUntil(global.clients.claim()));
-
-
-// Home router
-
-// Home
-
-self.toolbox.router.get('/', self.toolbox.networkFirst, {
-  cache: {
-    networkTimeoutSeconds: 3,
-    name: 'HomeCache',
-    maxEntries: 50,
-    maxAgeSeconds: 1 * 24 * 60 * 60
-  }
-});
-
-//Admin Ajax
-
-self.toolbox.router.get('/wp-admin/admin-ajax.php', self.toolbox.networkFirst, {
-  cache: {
-    networkTimeoutSeconds: 3,
-    name: 'HomeCache',
-    maxEntries: 50,
-    maxAgeSeconds: 1 * 24 * 60 * 60
-  }
-});
-
-// Home offline
-
-self.toolbox.router.get('/offline', self.toolbox.cacheOnly, {
-  cache: {
-    name: 'HomeCache',
-    maxEntries: 50,
-    maxAgeSeconds: 30 * 24 * 60 * 60
-  }
-});
-
-// Pages Home
-
-self.toolbox.router.get('/tip-us', self.toolbox.networkFirst, {
-  cache: {
-    networkTimeoutSeconds: 3,
-    name: 'HomeCache',
-    maxEntries: 50,
-    maxAgeSeconds: 30 * 24 * 60 * 60
-  }
-});
-self.toolbox.router.get('/contact', self.toolbox.networkFirst, {
-  cache: {
-    networkTimeoutSeconds: 3,
-    name: 'HomeCache',
-    maxEntries: 50,
-    maxAgeSeconds: 30 * 24 * 60 * 60
-  }
-});
-self.toolbox.router.get('/about', self.toolbox.networkFirst, {
-  cache: {
-    networkTimeoutSeconds: 3,
-    name: 'HomeCache',
-    maxEntries: 50,
-    maxAgeSeconds: 30 * 24 * 60 * 60
-  }
-});
-self.toolbox.router.get('/terms-and-conditions', self.toolbox.networkFirst, {
-  cache: {
-    networkTimeoutSeconds: 3,
-    name: 'HomeCache',
-    maxEntries: 50,
-    maxAgeSeconds: 30 * 24 * 60 * 60
-  }
-});
-self.toolbox.router.get('/privacy-policy', self.toolbox.networkFirst, {
-  cache: {
-    networkTimeoutSeconds: 3,
-    name: 'HomeCache',
-    maxEntries: 50,
-    maxAgeSeconds: 30 * 24 * 60 * 60
-  }
-});
-
-// Home png
-
-self.toolbox.router.get('/TechNuttyLogo.png', self.toolbox.cacheFirst, {
-  cache: {
-    name: 'HomeCache',
-    maxEntries: 50,
-    maxAgeSeconds: 30 * 24 * 60 * 60
-  }
-});
-
-// Home svg
-
-self.toolbox.router.get('/TechNuttyLogo.svg', self.toolbox.cacheFirst, {
-  cache: {
-    name: 'HomeCache',
-    maxEntries: 50,
-    maxAgeSeconds: 30 * 24 * 60 * 60
-  }
-});
-
-// PageSpeed
-
-self.toolbox.router.get('pagespeed_static/*', self.toolbox.cacheFirst, {
-  cache: {
-    name: 'PageSpeed',
-    maxEntries: 50,
-    maxAgeSeconds: 30 * 24 * 60 * 60
-  }
-});
-
-// Articles router
-
-global.toolbox.router.get('/articles/(.*)', function(request, values, options) { 
-  return global.toolbox.networkFirst(request, values, options).catch(function(error) {
-    if (request.method === 'GET' && request.headers.get('accept').includes('text/html')) {
-      return toolbox.cacheOnly(new Request('offline'), values, options);
+(global=>{
+    'use strict';
+    
+    // SW Toolbox
+    
+    importScripts('sw-toolbox.js');
+    
+    // Google Analytics
+    
+    self.goog = {
+        DEBUG: true
+    };
+    importScripts('offline-google-analytics-import.js');
+    goog.offlineGoogleAnalytics.initialize({});
+    
+    //Debug
+    
+    global.toolbox.options.debug = false;
+    
+    // Precache
+    
+    const FILES_TO_PRECACHE = ['/', '/wp-admin/admin-ajax.php', '/offline', '/wp-content/uploads/2016/06/25152352/215328-200.png', '/OFFLINEIMG.png', '/TechNuttyLogo.svg', '/TechNuttyLogo.png', '/wp-content/themes/TechNutty5-5/library/css/stylesheet.shorthand.min.css?v=3.0.7', '/wp-content/themes/TechNutty5-5/library/js/scripts.combined.min.js?v=1.1', '/wp-includes/js/jquery/jquery.js', '/wp-includes/js/jquery/jquery-migrate.min.js', '/modernizr-custom.js', '/topics/articles/all/news/', '/topics/articles/all/reviews/', '/merged-single-plugin.css'];
+    global.toolbox.precache(FILES_TO_PRECACHE);
+    
+    // Install and activate events
+    
+    self.addEventListener('install', function(event) {
+         console.log("\n%cYour requests... give them to me, now. \n","font-family: 'Open Sans',sans-serif; font-size: 11px; color: #333");
+         event.waitUntil(self.skipWaiting());
+    });
+    this.addEventListener('activate', function(event) {
+    var cacheWhitelist = ['v1.9.6'];
+    event.waitUntil(
+      caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log("\n%cDeleting inferior cache. \n","font-family: 'Open Sans',sans-serif; font-size: 11px; color: #333");
+          return caches.delete(key);
+        }
+      }));
+      })
+    );
+    });
+    
+     //Offline cover URL
+    
+    const OFFLINE_URL = '/offline';
+    self.addEventListener('fetch', event=>{
+        if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+            console.log('Serving page:', event.request.url);
+            event.respondWith(fetch(event.request).catch(()=>caches.match(OFFLINE_URL)));
+        }
     }
-    throw error;
-  });
-}, {networkTimeoutSeconds: 3, name: 'ArticlesCache', maxEntries: 50, maxAgeSeconds: 1 * 24 * 60 * 60});
-
-// Topics router
-
-global.toolbox.router.get('/topics/(.*)', function(request, values, options) { 
-  return global.toolbox.networkFirst(request, values, options).catch(function(error) {
-    if (request.method === 'GET' && request.headers.get('accept').includes('text/html')) {
-      return toolbox.cacheOnly(new Request('offline'), values, options);
-    }
-    throw error;
-  });
-}, {networkTimeoutSeconds: 3, name: 'TopicsCache', maxEntries: 50, maxAgeSeconds: 1 * 24 * 60 * 60});
-
-
-// Main Assets CSS router
-
-self.toolbox.router.get('/wp-content/themes/TechNutty5-5/library/css/(.*)', self.toolbox.cacheFirst, {  
-  cache: {
-    name: 'staticMainAssetsCache',
-    maxEntries: 50
-  }
-});
-
-// Main Assets JS router
-
-self.toolbox.router.get('/wp-content/themes/TechNutty5-5/library/js/(.*)', self.toolbox.cacheFirst, {  
-  cache: {
-    name: 'staticMainAssetsCache',
-    maxEntries: 50
-  }
-});
-
-// Main Assets Includes JS router
-
-self.toolbox.router.get('/wp-includes/js/(.*)', self.toolbox.cacheFirst, {  
-  cache: {
-    name: 'staticMainAssetsCache',
-    maxEntries: 50
-  }
-});
-
-// WP Plugins CSS Router
-
-self.toolbox.router.get('/wp-content/plugins/(.css)', self.toolbox.cacheFirst, {  
-    cache: {
-      name: 'content-css-js-cache-v1',
-      maxEntries: 50
-    }
-  }
-);
-
-// WP Plugins JS Router
-
-self.toolbox.router.get('/wp-content/plugins/(.js)', self.toolbox.cacheFirst, {  
-    cache: {
-      name: 'content-css-js-cache-v1',
-      maxEntries: 50
-    }
-  }
-);
-
-// WP Uploads Router
-
-self.toolbox.router.get('/wp-content/uploads/(.*)', self.toolbox.cacheFirst, {  
-    cache: {
-      name: 'content-cache-v1',
-      maxEntries: 50
-    }
-  }
-);
-
-// CDN Router
-
-self.toolbox.router.get('/wp-content/uploads/(.*)', self.toolbox.cacheFirst, {  
-  origin: /mufasa\.technutty\.co.uk/,
-  cache: {
-    name: 'CDNcache',
-    maxEntries: 30,
-    maxAgeSeconds: 1 * 24 * 60 * 60
-  }
-});
-
-
-//Third party routers
-
-//Font Icons
-
-self.toolbox.router.get('/f07dc75f.js', self.toolbox.cacheFirst, {  
-  origin: /use\.fonticons\.com/,
-  cache: {
-    name: 'dynamic-vendor-cache-v1',
-    maxEntries: 10
-  }
-});
-
-self.toolbox.router.get('/kits/*', self.toolbox.cacheFirst, {  
-  origin: /fonticons-free-fonticons\.netdna-ssl\.com/,
-  cache: {
-    name: 'dynamic-vendor-cache-v1',
-    maxEntries: 10
-  }
-});
-
-// Headway
-
-self.toolbox.router.get('/*', self.toolbox.networkFirst, {  
-  origin: /cdn\.headwayapp\.co/,
-  cache: {
-    networkTimeoutSeconds: 3,
-    name: 'dynamic-vendor-cache-v1',
-    maxEntries: 10
-  }
-});
-
-// Emotify
-
-self.toolbox.router.get('/api/articles/(.*)', self.toolbox.cacheFirst, {  
-  origin: /www\.goemotify\.com/,
-  cache: {
-    name: 'dynamic-emotify-cache-v1',
-    maxEntries: 30
-  }
-});
-
-})(self);
+    );
+    
+    // Offline fallback
+    
+    const addFallback = (cacheFunc, offlineAsset) => {
+    return (request, values, options) => {
+    return cacheFunc(request, values, options)
+    .catch(err => {
+      console.log('Serving offline asset:', event.request.url);
+      return fetch(offlineAsset);
+    });
+    };
+    };
+    
+    // Main Assets
+    
+    self.toolbox.router.get('/TechNuttyLogo.png', self.toolbox.cacheFirst, {
+        cache: {
+            name: 'staticMainAssetsCache',
+            maxEntries: 50,
+            maxAgeSeconds: 30 * 24 * 60 * 60
+        }
+    });
+    self.toolbox.router.get('/TechNuttyLogo.svg', self.toolbox.cacheFirst, {
+        cache: {
+            name: 'staticMainAssetsCache',
+            maxEntries: 50,
+            maxAgeSeconds: 30 * 24 * 60 * 60
+        }
+    });
+    self.toolbox.router.get('pagespeed_static/(.*)', self.toolbox.cacheFirst, {
+        cache: {
+            name: 'PageSpeed',
+            maxEntries: 50,
+            maxAgeSeconds: 30 * 24 * 60 * 60
+        }
+    });
+    
+    // Static Assets
+    
+    self.toolbox.router.get('/wp-content/themes/TechNutty5-5/library/css/(.*)', self.toolbox.cacheFirst, {
+        cache: {
+            name: 'staticMainAssetsCache',
+            maxEntries: 50
+        }
+    });
+    self.toolbox.router.get('/wp-content/themes/TechNutty5-5/library/js/(.*)', self.toolbox.cacheFirst, {
+        cache: {
+            name: 'staticMainAssetsCache',
+            maxEntries: 50
+        }
+    });
+    self.toolbox.router.get('/wp-includes/js/(.*)', self.toolbox.cacheFirst, {
+        cache: {
+            name: 'staticMainAssetsCache',
+            maxEntries: 50
+        }
+    });
+    self.toolbox.router.get('/wp-content/plugins/*(.*)', self.toolbox.cacheFirst, {
+        cache: {
+            name: 'staticMainAssetsCachePlugins',
+            maxEntries: 50
+        }
+    });
+    self.toolbox.router.get('/wp-content/plugins/*(.*)', self.toolbox.cacheFirst, {
+        cache: {
+            name: 'staticMainAssetsCachePlugins',
+            maxEntries: 50
+        }
+    });
+    
+    // Pages cache
+    
+    self.toolbox.router.get('/', addFallback(self.toolbox.networkFirst, '/offline'), {
+        cache: {
+            networkTimeoutSeconds: 3,
+            name: 'pagesCache',
+            maxEntries: 50,
+            maxAgeSeconds: 1 * 24 * 60 * 60
+        }
+    });
+    
+    // Image Caches
+    
+    self.toolbox.router.get('/wp-content/uploads/(.*)', addFallback(self.toolbox.cacheFirst, '/OFFLINEIMG.png'), {
+        cache: {
+        networkTimeoutSeconds: 3,
+        name: 'imageCache',
+        maxEntries: 50,
+        maxAgeSeconds: 1 * 24 * 60 * 60
+        }
+    });
+    self.toolbox.router.get('/wp-content/uploads/(.*)', addFallback(self.toolbox.cacheFirst, '/OFFLINEIMG.png'), {
+        origin: /mufasa\.technutty\.co.uk/,
+        cache: {
+        networkTimeoutSeconds: 3,
+        name: 'cdnCache',
+        maxEntries: 50,
+        maxAgeSeconds: 1 * 24 * 60 * 60
+        }
+    });
+    
+    // Third party caches
+    
+    self.toolbox.router.get('/css', self.toolbox.cacheFirst, {
+        origin: /fonts\.googleapis\.com/,
+        cache: {
+            name: 'staticMainAssetsCacheThirdParty',
+            maxEntries: 10,
+            maxAgeSeconds: 1 * 24 * 60 * 60
+        }
+    });
+    self.toolbox.router.get('/(.*)', self.toolbox.cacheFirst, {
+        origin: /use\.fonticons\.com/,
+        cache: {
+            name: 'staticMainAssetsCacheThirdParty',
+            maxEntries: 10,
+            maxAgeSeconds: 1 * 24 * 60 * 60
+        }
+    });
+    self.toolbox.router.get('/kits/*', self.toolbox.cacheFirst, {
+        origin: /fonticons-free-fonticons\.netdna-ssl\.com/,
+        cache: {
+            name: 'staticMainAssetsCacheThirdParty',
+            maxEntries: 10,
+            maxAgeSeconds: 1 * 24 * 60 * 60
+        }
+    });
+}
+)(self);
